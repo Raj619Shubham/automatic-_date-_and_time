f2f03e3ac98250fc638c441b1c8af7451016a3a2
Raj Shubham <sraj17297@gmail.com>
2021-01-02 23:36:02 +0530

    this is a simple greeting website

--- src/index.js
+++ src/index.js
@@ -3,12 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+var curDate= new Date(2021,1,2,13);
+curDate= curDate.getHours();
+var greetings='';
+const cssStyle = {};
+if(curDate >=1 && curDate<=12){
+  greetings="Good morning";
+  cssStyle.color = "blue";
+}else if(curDate >=12 && curDate <19){
+  greetings="Good Afternoon";
+  cssStyle.color = "orange";
+}else{
+  greetings="Good night";
+  cssStyle.color = 'brown';
+}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <>
+  <div>
+<h1>
+  Hello everyone, <span style={cssStyle}> {greetings}</span>
+</h1>,
+</div>
+</>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
